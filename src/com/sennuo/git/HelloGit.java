9564d996190baf9c0a186ca4643899a9b6ad643e
onewayWang <wang1990080@163.com>
2016-09-19 11:25:34 +0800

    Update HelloGit.java
    测试冲突

--- src/com/sennuo/git/HelloGit.java
+++ src/com/sennuo/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
 	public void testPush(){
 		System.out.println("test git push order");
 		System.out.println("hello,I have accessed it!")；
+		System.out.prinyly("I am testing conflick!);
 		System.out.println("I have modified it,please sync!")；
 	}
 }

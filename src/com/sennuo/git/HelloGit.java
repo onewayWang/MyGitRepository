67b782e30ee266491e6181010182bbf9a4a71df0
wangwei <741217349@qq.com>
2016-09-19 11:43:26 +0800

    55

--- src/com/sennuo/git/HelloGit.java
+++ src/com/sennuo/git/HelloGit.java
@@ -7,10 +7,9 @@ public class HelloGit {
 		System.out.println("hello,I have accessed it!")；
 		System.out.prinyly("I am testing conflick!);
 		System.out.println("I have modified it,please sync!")；
-<<<<<<< HEAD
-		System.out.println("haha111 !");
-=======
+
+
 		System.out.println("I have modified it,please sync!")；
->>>>>>> dc361797a63bcf775df019714daa7730658e6260
+
 	}
 }
